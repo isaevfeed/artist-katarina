a305e2557901b4a4dcca32c797ab5e77bbab9b2e
mihailisaev <isaevfeed@gmail.com>
2020-11-15 14:52:53 +0300

    заверстал раздел работ

--- app/js/common.js
+++ app/js/common.js
@@ -4,11 +4,13 @@ $(function() {
         const scrollPos = $(document).scrollTop();
         decoreLineMove('top', scrollPos);
     });
-	
+    
 });
 
 function decoreLineMove(pos, val) {
     const decorLine = $('.decor-line');
+    const decoreNav = $('.nav-decore-inside');
     
     decorLine.css(pos, -1 * (val / 50));
+    decoreNav.css(pos, -1 * (val / 10));   
 }
